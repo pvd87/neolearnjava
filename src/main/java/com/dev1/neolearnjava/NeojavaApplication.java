5e1ae7893d6f87bb61e3b58ef4111b4b055d6dd6
Phuong VD <phuongvd@dnkinno.com>
2022-06-14 23:14:06 +0700

    init

---
+++ src/main/java/com/dev1/neolearnjava/NeojavaApplication.java
@@ -0,0 +1,13 @@
+package com.dev1.neolearnjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev1/neolearnjava/NeojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev1.neolearnjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
